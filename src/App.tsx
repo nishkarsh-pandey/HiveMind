1d8d63ee783187e2af9bb7ec69a62a78b9b976ba
nshkarshpandey <nishkarsh0886.be20@chitkara.edu.in>
2024-06-18 12:23:45 +0530

    Added Tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import {Routes , Route } from 'react-router-dom'
+
 import './global.css'
+import SignInForm from './_auth/Forms/SignInForm'
+import {Home} from './_root/pages'
+import SignUpForm from './_auth/Forms/SignUpForm'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
 
 const App = () => {
   return (
-    <div>
-      APP
-    </div>
+    <main className='flex h-screen'>
+      <Routes>
+        {/* Public Routes */}
+        <Route element = {<AuthLayout/>}>
+
+        <Route path="/sign-in" element={<SignInForm />}  />
+        <Route path="/sign-up" element={<SignUpForm />}  />
+
+        </Route>
+
+        {/* private routes */}
+          <Route element={<RootLayout />}>
+              <Route index element ={<Home />}/>
+          </Route>
+
+        
+
+      </Routes>
+    </main>
   )
 }
 
